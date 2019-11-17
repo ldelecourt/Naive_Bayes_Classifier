4d12214b84c1da805cf96b67b667aba9d3d01a94
ldelecourt <leo.delecourt@gmail.com>
2019-11-17 20:18:53 +0100

    table probabilité conditionnelle

--- v0/Naive_Bayes.R
+++ v0/Naive_Bayes.R
@@ -1,15 +1,15 @@
-#library(readxl)
+library(readxl)
 
-#  On charge les données
+#  On charge les donn?es
 data <- read_excel("/Users/d/Cours/SISE_M2/programmation_R/projet/heart.xls", sheet='dataset')
 
-# On prend que les variables qualitatives pour ces données (Census)
+# On prend que les variables qualitatives pour ces donn?es (Census)
 #data <- read_excel("/Users/d/Cours/SISE_M2/programmation_R/projet/Census.xlsx", sheet='adult')
 #data <- data[c(-1, -9)]
 
 
 
-# Création du modèle (fit())
+# Cr?ation du mod?le (fit())
 
 fit <- function(formula, data, laplace=1, ...) {
   # Nom de l'objet en sortie
@@ -22,33 +22,38 @@ fit <- function(formula, data, laplace=1, ...) {
   # On met toutes les colonnes en Factor
   df <- as.data.frame(lapply(df, factor))
   
-  # Probabilités des modalités de chaque variable explicative en fonction de la variable expliquée
+  # Probabilit?s des modalit?s de chaque variable explicative en fonction de la variable expliqu?e
   list_proba_explicative <- c()
-  NBAYES$table_proba_cond <- c()
+  #NBAYES$table_proba_cond <- c()
+  
   for (i in 2:ncol(df)) {
-    # Table de fréquence
+    # Table de fr?quence
     table_freq <- table(df[ ,i], df[ ,1])
-    # Si une modalité à une fréquence == 0
+    # Si une modalit? ? une fr?quence == 0
     if (sum(table_freq == 0 ) > 0) { 
       warning("Laplace smoothing had to be used for variable: '", colnames(df[i]) ,"' !!!\n")
       # Lissage de Laplace
-      table_freq[which(table_freq == 0)] <- 1   # On ajoute 1 à la fréquence == 0
+      table_freq[which(table_freq == 0)] <- 1   # On ajoute 1 ? la fr?quence == 0
     }
     proba_explicative <- prop.table(table_freq, 2)
+    
+    NBAYES$table_proba_cond[[colnames(df[i])]] <- prop.table(table_freq, 2)
+    
     #NBAYES$table_proba_cond[i] <- as.matrix(proba_explicative, rownames=rownames(data))
     list_proba_explicative <- rbind(list_proba_explicative, proba_explicative)
-    # Modalités des variables explicatives en lignes
-    # Somme des proba des modalités d'une même variable = 1
+    # Modalit?s des variables explicatives en lignes
+    # Somme des proba des modalit?s d'une m?me variable = 1
     # P(no/negative) + P(yes/negative) = 1
   }
   
-  # Probabilités des modalités de la variable expliquée
+  # Probabilit?s des modalit?s de la variable expliqu?e
   proba_Y <- prop.table(table(df[1]))
+  NBAYES$table_explicative <- proba_Y
   
-  # Matrice avec toutes les probabilités conditionnelles + probabilités variables expliquée
+  # Matrice avec toutes les probabilit?s conditionnelles + probabilit?s variables expliqu?e
   list_proba_all <- rbind(list_proba_explicative, proba_Y)
-  
-  # On implémente les instances de la classe NBAYES
+    
+  # On impl?mente les instances de la classe NBAYES
   NBAYES$list_proba_all <- list_proba_all
   class(NBAYES) <- "NBAYES"
   return(NBAYES)
@@ -59,12 +64,13 @@ predict <- function(object_NBAYES, data) {       # ajouter une option type('clas
   if (class(object_NBAYES) != "NBAYES") {
     stop("The object you gave is not a NBAYES object")
   }
-  # Prédiction grâce au modèle (predict())
+  # Pr?diction gr?ce au mod?le (predict())
   proba_finale <- apply(object_NBAYES$list_proba_all, 2, prod)
-  # On normalise pour avoir une probabilité 0 < ? < 1
+  # On normalise pour avoir une probabilit? 0 < ? < 1
   proba_finale <- proba_finale / sum(proba_finale)
   
-  return(list(proba_explicative=object_NBAYES$list_proba_all, proba_finale=proba_finale))
+  return(list(table_cond=object_NBAYES$table_proba_cond, table_explicative=object_NBAYES$table_explicative, proba_finale=proba_finale)) 
+  #proba_explicative=object_NBAYES$list_proba_all,
 }
 
 
